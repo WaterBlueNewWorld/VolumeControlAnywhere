8b4c00e2bdd138b56b10e42a5a0c0914d861ca3a
Max Lopez <darkar.zero117@gmail.com>
2025-08-14 02:31:32 -0600

    Audio device selector added, changed the color of the background, every time you change the audio device the app list gets refreshed so it doesn't overpopulate

--- VolumeControlAnywhere/Form1.cs
+++ VolumeControlAnywhere/Form1.cs
@@ -23,13 +23,64 @@ public partial class Form1 : Form
         };
         _trackBarVolumen = new List<Control>();
         InitializeComponent();
-        InicializarListaProgramas();
+        Load += Form1_Load;
+        
+        
         FormClosing += (s, e) =>
         {
             // Unregister hotkey if needed
             GlobalHotkey.UnregisterHotKey(Handle, GlobalHotkey.HotkeyId);
         };
         GlobalHotkey.RegisterHotKey(Handle, GlobalHotkey.HotkeyId, GlobalHotkey.ModControl | GlobalHotkey.ModShift, (int)Keys.B);
+    }
+
+    private void Form1_Load(object? sender, EventArgs e)
+    {
+        InicializarListaProgramas();
+        CalculateWindowPos();
+        
+        MMDeviceEnumerator enumDisp = new MMDeviceEnumerator();
+        MMDevice defaultDevice = enumDisp.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+        string defaultDeviceName = defaultDevice.FriendlyName;
+        for (int i = 0; i < deviceSelector.Items.Count; i++)
+        {
+            if (deviceSelector.Items[i] is MMDevice dev && dev.FriendlyName == defaultDeviceName)
+            {
+                deviceSelector.SelectedIndex = i;
+                break;
+            }
+        }
+    }
+    
+    private void CalculateWindowPos()
+    {
+        Screen currentScreen = Screen.FromPoint(Cursor.Position);
+        Rectangle screenBounds =  currentScreen.WorkingArea;
+        Point mousePos = MousePosition;
+        
+        StartPosition = FormStartPosition.Manual;
+        
+        int mousePosX = mousePos.X;
+        int mousePosY = mousePos.Y;
+        
+        if(mousePosX + ClientSize.Width > screenBounds.Right)
+        {
+            mousePosX = mousePos.X - ClientSize.Width;
+        }
+        if(mousePosY + ClientSize.Height > screenBounds.Bottom)
+        {
+            mousePosY = mousePos.Y - ClientSize.Height;
+        }
+        if(mousePosX < screenBounds.Left)
+        {
+            mousePosX = screenBounds.Left;
+        }
+        if(mousePosY < screenBounds.Top)
+        {
+            mousePosY = screenBounds.Top;
+        }
+        
+        Location = new Point(mousePosX, mousePosY);
     }
     
     protected override void WndProc(ref Message m)
@@ -43,7 +94,7 @@ public partial class Form1 : Form
             }
             else
             {
-                InitializeComponent();
+                CalculateWindowPos();
                 Show();
                 BringToFront();
             }
@@ -54,26 +105,37 @@ public partial class Form1 : Form
     private void InicializarListaProgramas()
     {
         Controls.Add(_listaProgramas);
-
-        // _timer = new Timer();
-        // _timer.Interval = 1000;
-        // _timer.Tick += (s, e) => RefrescarListaProgramas();
-        // _timer.Start();
-
-        ListOpenApps();
+        
+        //Get all the audio render devices
+        MMDeviceEnumerator enumDisp = new MMDeviceEnumerator();
+        MMDeviceCollection deviceCollection = enumDisp.EnumerateAudioEndPoints(DataFlow.Render, (DeviceState)Role.Multimedia);
+        Console.WriteLine("HAY "+deviceCollection.Count+" DISPOSITIVOS DE SALIDA");
+        
+        foreach (MMDevice device in deviceCollection)
+        {
+            deviceSelector.Items.Add(device);
+        }
+        deviceSelector.DisplayMember = "FriendlyName";
+        
+        deviceSelector.SelectedIndexChanged += (s, e) =>
+        {
+            MMDevice selectedDevice = deviceSelector.SelectedItem as MMDevice;
+            int selectedIndex = deviceSelector.SelectedIndex;
+            Console.WriteLine($"Selected index: {selectedIndex}");
+            Console.WriteLine(selectedDevice.FriendlyName);
+            ListOpenApps(selectedDevice);
+        };
     }
 
-    private void ListOpenApps()
+    private void ListOpenApps(MMDevice selectedDevice)
     {
         if (_listaProgramas == null) return;
+        _trackBarVolumen.ForEach(c => c.Dispose());
+        _trackBarVolumen.Clear();
         _listaProgramas.Controls.Clear();
+        VolumeSlider.AppVolumes.Clear();
         
-        MMDeviceEnumerator enumDisp = new MMDeviceEnumerator();
-        MMDevice defaultDevice = enumDisp.GetDefaultAudioEndpoint(
-            DataFlow.Render,
-            Role.Multimedia
-        );
-        SessionCollection sessionsAudio = defaultDevice.AudioSessionManager.Sessions;
+        SessionCollection sessionsAudio = selectedDevice.AudioSessionManager.Sessions;
 
         for (int i = 0; i < sessionsAudio.Count; i++)
         {
@@ -86,4 +148,5 @@ public partial class Form1 : Form
         }
         _listaProgramas.Controls.AddRange(_trackBarVolumen.ToArray());
     }
+    
 }
